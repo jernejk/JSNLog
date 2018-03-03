674d4d9f291101a986c7b36953002d5be97dae19
Jernej Kavka (JK) <jernej.kavka@outlook.com>
2018-03-03 17:47:46 +1000

    Very basic JSNLog implementation.

--- JSNLog.Web/src/app/app.module.ts
+++ JSNLog.Web/src/app/app.module.ts
@@ -2,12 +2,40 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageWithErrorComponent } from './page-with-error/page-with-error.component';
+import { JL } from 'jsnlog';
+
+// Logging stuff
+class UncaughtExceptionHandler implements ErrorHandler {
+  handleError(error: any) {
+      JL().fatalException('Uncaught Exception', error);
+      console.warn('logged');
+  }
+}
+
+const logLevel = JL.getAllLevel();
+const appender = JL.createAjaxAppender('example appender');
+appender.setOptions({
+    'bufferSize': 20,
+    'storeInBufferLevel': 1000,
+    'level': logLevel,
+    'sendWithBufferLevel': 6000,
+    'url': 'http://localhost:51213/jsnlog.logger'
+});
+
+// Configure the JSNLog logging library.
+// See http://jsnlog.com/Documentation/JSNLogJs
+JL().setOptions({
+    'appenders': [appender],
+    'level': logLevel
+});
+
+JL().info('Angular is starting...');
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -28,7 +56,10 @@ const appRoutes: Routes = [
     CommonModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    { provide: 'JSNLOG', useValue: JL },
+    { provide: ErrorHandler, useClass: UncaughtExceptionHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
