ad2e07511859ef112863fa9aef1ba91bc47db496
Jernej Kavka (JK) <jernej.kavka@outlook.com>
2018-03-03 15:59:15 +1000

    Added new Angular app with login page

---
+++ JSNLog.Web/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
